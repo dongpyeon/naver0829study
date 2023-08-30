784c6f50704ee3b2cac3c8ba353d22d152445aa1
dong <kd0948@gmail.com>
2023-08-30 16:14:35 +0900

    자바프로젝트생성

--- javawork/naverjavaproject/src/bit701/day0830/DataEx1.java
+++ javawork/naverjavaproject/src/bit701/day0830/DataEx1.java
@@ -4,10 +4,22 @@ public class DataEx1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//한글 
-		System.out.println("hello!11");
-		System.out.println("happy!2232");
 		
+		
+		//printf: %d f %c %s
+		//\n 줄넘김  \t 다음탭
+		int age =23;
+		double weight = 56.7;
+		char blood = 'A';
+		String name="캐서린";
+		System.out.printf("이름:%s\n",name);
+		System.out.printf("나이: %d세",age);
+		System.out.printf("몸무게: %fKg",weight);
+		System.out.printf("몸무게: %1.2fKg",weight);
+		System.out.printf("혈액형: %c\n",blood);
+		System.out.println("=======================");
+		System.out.printf("이름 :%s\n나이 :%d\n몸무게 :%1.1fKg\n혈액형 :%c",name,age,weight,blood);
+
 	}
 
 }
